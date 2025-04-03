7077bd04444fc59c8129421cec9d9ac4293c035f
rejiegulay <gulayrejie@gmail.com>
2025-04-03 10:41:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -57,14 +57,14 @@
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
-          <a class="navbar-brand" href="index.html"><img src="assets/images/DSDLOGO1.png"></a>
+          <a class="navbar-brand" href="index.php"><img src="assets/images/DSDLOGO1.png"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home
+                <a class="nav-link" href="index.php">Home
                   <span class="sr-only">(current)</span>
                 </a>
               </li>
